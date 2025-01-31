277ea8e31eb1b8edce70f8a23bbad336d7a5124e
Alvaro Royo <alvaroroyo3@gmail.com>
2025-01-31 10:39:23 +0100

    Remove folder

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ARFirebaseManager"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
